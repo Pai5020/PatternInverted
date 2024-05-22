c565fd3c223157bf35d02d102a8913269f3e6e92
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-05-22 21:26:52 +0530

    revised

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,30 @@ public class Main {
     public static void main(String[] args) {
         int i, j,k;
 
-        for (i= 0; i<=6 ; i++)
-        {
-            for (j=i; j <6 ;j++)
-            {
-                System.out.print(" ");
+        for (i= 0; i<=6 ; i++) {
+
+            if (i % 2 != 0) {
+
+                for (j = i; j < 6; j++) {
+                    System.out.print(" ");
+                }
+
+                for (k = 1; k <= i; k++) {
+                    System.out.print("*");
+                }
+                System.out.println(" ");
             }
-            for (k=1; k<=i;k++)
-            {
-                System.out.print("*");
-            }
-            System.out.println(" ");
         }
-        for (i=6; i>=0; i--)
-        {
-            for(j=i; j<=6;j++)
-            {
-                System.out.print(" ");
+        for (i=5; i>=1; i--) {
+            if (i % 2 != 0) {
+                for (j = i; j <= 5; j++) {
+                    System.out.print(" ");
+                }
+                for (k = 1; k <= i; k++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
             }
-            for(k=1; k<i ;k++)
-            {
-                System.out.print("*");
-            }
-            System.out.println(" ");
         }
     }
 }
