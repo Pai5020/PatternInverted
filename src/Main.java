749eaca843de468e2d4be85278f823a0555604d9
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-05-23 14:13:08 +0530

    revised 3

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,32 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int i, j;
+        int i, j, N;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no of rows : ");
+        N = sc.nextInt();
 
-        for (i = 1; i <= 7; i += 2) {
-            for (j = 1; j <= 7; j++) {
-                if (j < 7 - i + 1) {
-                    System.out.print(" ");
-                } else {
+        for (i = 1; i <= N; i += 2) {
+            for (j = 1; j <= 2 * N; j++) {
+                if (j <= i || j > 2 * N - i) {
                     System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
         }
 
-        for (i = 7; i >= 1; i -= 2) {
-            for (j = 1; j <= 7; j++) {
-                if (j <= 7 - i) {
-                    System.out.print(" ");
-                } else {
+        for (i = N-2; i >= 1; i -= 2) {
+            for (j = 1; j <= 2 * N; j++) {
+                if (j <= i || j > 2 * N - i) {
                     System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
         }
     }
 }
+
